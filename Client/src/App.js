3d3e389ddc30617a3ff53755352ba9dbdba24387
Samuel <samuelsf@hotmail.co.uk>
2022-08-19 17:31:25 +0100

    was able to implement two pages (home, and europe)

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import Home from "./continents/Home";
+import Europe from "./continents/Europe";
 
 const App = () => {
   
@@ -13,6 +14,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={< Home />} />
+        <Route path="/europe" element={< Europe />} />
       </Routes>
     </Router>
   );
