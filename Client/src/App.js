1e8e91a28bf1d6ab82565c37558c3b93eecce275
Samuel <samuelsf@hotmail.co.uk>
2022-08-19 17:23:37 +0100

    added home page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './App.css';
 
-function App() {
+import Home from "./continents/Home";
+
+const App = () => {
+  
+  
+  
   return (
-    <div className="App">
-      <p>Hello world!</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={< Home />} />
+      </Routes>
+    </Router>
   );
 }
 
