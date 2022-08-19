ace9d36504f847c13eae4f8aba1cae28cf0e6628
Naefunn <nathan.d.foster1@gmail.com>
2022-08-19 12:10:37 +0100

    database structure added

---
+++ Server/db/seeds.js
@@ -0,0 +1,495 @@
+use countryHub;
+db.dropDatabase();
+
+db.countries.insertMany([
+// Europe
+
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Europe"
+    },
+// South America
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South American"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "South America"
+    },
+// Asia
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Asia"
+    },
+// Africa 
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Africa"
+    },
+// North and Central America
+
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "North and Central America"
+    },
+// Oceana 
+
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+    {
+        name: "",
+        population: 0,
+        currency: "",
+        language: "",
+        captial: "",
+        continent: "Oceana"
+    },
+
+])
