3e3bb43b33683670a69ab5d1196f9c1ac22f5a1c
Lewis <lewisdsykes@gmail.com>
2022-08-21 16:38:51 +0100

    added map to home

--- Client/src/continents/Home.js
+++ Client/src/continents/Home.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { memo } from "react";
+import {
+  ZoomableGroup,
+  ComposableMap,
+  Geographies,
+  Geography
+} from "react-simple-maps";
 
-const Home = () => (
-  <div>
-    <h4>Home</h4>
-    <p>Welcome to our magical homepage</p>
-  </div>
-);
+import "./styles.css";
 
-export default Home;
+const Home = ({ setTooltipContent, onClick}) => {
+  return (
+    <div className="map" data-tip="">
+      <ComposableMap>
+        <ZoomableGroup>
+          <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => {
+                    setTooltipContent(`${geo.properties.continent}`);
+                  }}
+
+                  onClick={onClick}
+                    
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#D6D6DA",
+                      outline: "none"
+                    },
+                    hover: {
+                      fill: `${geo.properties.color}`,
+                      outline: "none",
+                      cursor: "pointer"
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none"
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
+  );
+};
+
+export default memo(Home);
