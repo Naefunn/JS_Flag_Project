038d085ab6306c0cc1f8c7a5dd74b6bdd679f3bf
Lewis <lewisdsykes@gmail.com>
2022-08-21 17:27:44 +0100

    added navbar options and css

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 import './App.css';
 
 import Home from "./continents/Home";
 import Europe from "./continents/Europe";
+
 
 const App = () => {
   
   
   
   return (
+    <>
+    <Navbar/>
     <Router>
       <Routes>
         <Route exact path="/" element={< Home />} />
         <Route path="/europe" element={< Europe />} />
       </Routes>
     </Router>
+    </>
   );
 }
 
---
+++ Client/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Navbar.css';
+
+
+const Navbar = () => {
+
+
+
+    return (
+
+        <>  
+
+        <div className="Navbar">
+            <ul>
+                <li>Home</li>
+                <li>Quiz</li>
+            </ul>
+        </div>
+       
+         
+        </>
+
+    
+    )
+    
+    ;
+}
+
+
+export default Navbar;
