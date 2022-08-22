c2ab251f94457c951d97cd8968b153e2c40fe024
Lewis <lewisdsykes@gmail.com>
2022-08-22 15:03:46 +0100

    added the North America quiz

--- Client/src/continents/North&CentralAmerica.js
+++ Client/src/continents/North&CentralAmerica.js
@@ -1,8 +1,10 @@
 import React from "react";
+import NorthAmericaQuiz from "../components/North&CentralAmericaQuiz";
 
 const NorthAndCentralAmerica = () => (
   <div>
     <h4>North & Central America</h4>
+    <NorthAmericaQuiz/>
   </div>
 );
 
