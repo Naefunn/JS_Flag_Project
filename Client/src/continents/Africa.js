05b3050d6f1d1041e65f1b80ee172e24a102ab5a
Naefunn <nathan.d.foster1@gmail.com>
2022-08-22 15:31:21 +0100

    africa linked to homepage

--- Client/src/continents/Africa.js
+++ Client/src/continents/Africa.js
@@ -1,9 +1,15 @@
 import React from "react";
+import AfricaQuiz from "../components/AfricaQuiz";
 
-const Africa = () => (
+const Africa = () => {
+return (
+  <>
   <div>
     <h4>Africa</h4>
+    <AfricaQuiz/>
   </div>
-);
+  </>
+)
+};
 
 export default Africa;
