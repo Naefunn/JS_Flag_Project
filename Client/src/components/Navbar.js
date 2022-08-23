b4b05d3d697a0595b530607e848d09e313d2a8ee
Naefunn <nathan.d.foster1@gmail.com>
2022-08-23 11:49:49 +0100

    home button working

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -10,7 +10,9 @@ const Navbar = () => {
 
         <>  
         <div className="Navbar">
-            <a href="/"/>< img className="home" src="https://toppng.com/uploads/preview/earth-globe-vector-world-map-icon-transparent-11563460387dsa5p0tifc.png"/>
+            <ul>
+                <li><a href="/"><img className="home" src="https://toppng.com/uploads/preview/earth-globe-vector-world-map-icon-transparent-11563460387dsa5p0tifc.png"/></a></li>
+            </ul>
         </div>
         </>
     );
